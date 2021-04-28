26567063e929a20df8c66a01431c309c0161f0e1
Tenryu <tenflowwolfnet@gmail.com>
2021-04-28 12:43:35 +0900

    [Delete] コメントを削除

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,6 @@ const colorList = [
 
 /**
  * console.log 用のデコレーションメソッド
- *
- * 【使い方】
- *  1. 上記の colorList に定義された color をタグ名にしたタグで色を変更したい箇所を囲む
- *        hoge <red>piyo</red> huga
- *      このような文字列を引数に渡す
- *  2. 返り値を console.log() の引数にスプレッド構文で展開して渡す
- *      console.log(...LogDecorator('hoge <red>piyo</red> huga'))
- *
  * @param str
  * @constructor
  */
